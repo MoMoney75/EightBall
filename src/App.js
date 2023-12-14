efdc1e78fc114c088ec3442b477f70874561ffe9
Mauricio <maudusi12@gmail.com>
2023-12-13 20:48:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Eightball} from './Eightball';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Play a game of Eight Ball, ask away!
+      </h1>
+      
+      <Eightball/>
+         
+    </div>
+  );
+}
+
+export default App;
